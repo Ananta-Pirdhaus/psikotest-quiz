bf4cf5b58a76e676f682dd187bebdae0bbca1be2
Ananta Pirdhaus <ananta.firdaus2@gmail.com>
2025-02-18 16:31:24 +0700

    update: result quiz notifications

--- src/client/quiz/result/components/ResultQuzCard.jsx
+++ src/client/quiz/result/components/ResultQuzCard.jsx
@@ -1,35 +1,25 @@
-import React, { useState } from "react";
-import { Bar, Doughnut } from "react-chartjs-2";
+import React, { useState, useEffect } from "react";
+import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
   BarElement,
-  ArcElement,
   Title,
   Tooltip,
   Legend,
 } from "chart.js";
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
   BarElement,
-  ArcElement,
   Title,
   Tooltip,
   Legend
 );
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUser,
-  faGraduationCap,
-  faSchool,
-  faStar,
-  faChevronDown,
-  faChevronUp,
-} from "@fortawesome/free-solid-svg-icons";
-
 
 const ResultQuizCard = ({ quizResult }) => {
   if (!quizResult) return null;
@@ -49,35 +39,16 @@ const ResultQuizCard = ({ quizResult }) => {
         label: "Skor Talenta",
         data: talent.map((t) => t.score),
         backgroundColor: [
-          "#4bc0c0",
-          "#9966ff",
-          "#ff9f40",
-          "#c9cbcf",
-          "#2ecc71",
-          "#e74c3c",
-          "#f39c12",
-          "#8e44ad",
+          "#ff9f40", // Orange
+          "#f39c12", // Yellow
+          "#f9b5b1", // Light Red
+          "#fedc56", // Soft Yellow
+          "#ffacb7", // Light Pink
+          "#ff6f61", // Coral
+          "#f4b6c2", // Blush
+          "#f1a7a1", // Peach
         ],
         borderRadius: 10,
-      },
-    ],
-  };
-
-  const doughnutData = {
-    labels: talent.map((t) => t.name),
-    datasets: [
-      {
-        data: talent.map((t) => t.score),
-        backgroundColor: [
-          "#4bc0c0",
-          "#9966ff",
-          "#ff9f40",
-          "#c9cbcf",
-          "#2ecc71",
-          "#e74c3c",
-          "#f39c12",
-          "#8e44ad",
-        ],
       },
     ],
   };
@@ -97,114 +68,129 @@ const ResultQuizCard = ({ quizResult }) => {
     },
   };
 
+  useEffect(() => {
+    // Menampilkan toast ketika komponen pertama kali dirender
+    toast.warn(
+      "Jangan salah, ya! Bukan berarti kelima kecerdasan lainnya ini tidak kamu miliki, lho! Itu menandakan bahwa 3 kecerdasan unggulanmu sering kamu gunakan dalam belajar atau menyelesaikan sebuah masalah",
+      {
+        position: "top-right",
+        autoClose: 7500, // Toast akan muncul selama 30 detik
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        style: {
+          width: "80vw", // Menggunakan viewport width untuk lebar yang lebih responsif
+          maxWidth: "500px", // Lebar maksimum untuk tampilan besar
+          whiteSpace: "normal", // Membiarkan teks membungkus
+          wordWrap: "break-word", // Memecah kata panjang
+          textAlign: "start", // Menyejajarkan teks di kiri
+          padding: "10px", // Menambahkan sedikit ruang di dalam toast
+          boxSizing: "border-box", // Memastikan padding tidak mengubah ukuran total
+        },
+      }
+    );
+  }, []); // Hanya dijalankan sekali setelah pertama kali komponen dirender
+
   return (
-    <div className="max-w-3xl mx-auto p-6 bg-white shadow-2xl rounded-2xl border border-gray-200">
-      <h2 className="text-3xl font-bold text-gray-700 mb-6 text-center">
+    <div className="max-w-7xl mx-auto p-6 bg-white shadow-2xl rounded-2xl border border-gray-200">
+      <h2 className="text-4xl font-bold text-yellow-600 mb-6 text-center">
         Hasil Kuis
       </h2>
 
-      <div className="mb-6 p-6 bg-gradient-to-r from-orange-50 via-yellow-50 to-yellow-100 rounded-2xl shadow-lg border transition-transform transform hover:scale-105 w-full">
-        <h3 className="font-bold text-xl text-purple-800 flex items-center justify-center gap-3">
-          <FontAwesomeIcon icon={faUser} className="text-purple-600" />
-          Peserta
+      <div className="mb-6 p-6 bg-gradient-to-r from-yellow-200 via-orange-200 to-yellow-300 rounded-2xl shadow-lg border transition-transform transform hover:scale-105 w-full">
+        <h3 className="font-bold text-xl text-orange-700 flex items-center justify-center gap-3">
+          <i className="fas fa-user-circle text-xl text-orange-700"></i> Peserta
         </h3>
         <div className="mt-4 space-y-3 text-gray-800">
           <p className="flex flex-row items-center gap-3">
-            <FontAwesomeIcon icon={faUser} className="text-pink-500" />
-            <span className="font-semibold text-purple-800">Nama:</span>
+            <i className="fas fa-user text-yellow-700"></i>
+            <span className="font-semibold text-yellow-700">Nama:</span>
             <span className="text-gray-700">{participant.name}</span>
           </p>
           <p className="flex flex-row items-center gap-3">
-            <FontAwesomeIcon
-              icon={faGraduationCap}
-              className="text-green-500"
-            />
-            <span className="font-semibold text-purple-800">Kelas:</span>
+            <i className="fas fa-school text-yellow-700"></i>
+            <span className="font-semibold text-yellow-700">Kelas:</span>
             <span className="text-gray-700">{participant.class}</span>
           </p>
           <p className="flex flex-row items-center gap-3">
-            <FontAwesomeIcon icon={faSchool} className="text-indigo-500" />
-            <span className="font-semibold text-purple-800">Sekolah:</span>
+            <i className="fas fa-building text-yellow-700"></i>
+            <span className="font-semibold text-yellow-700">Sekolah:</span>
             <span className="text-gray-700">{participant.school}</span>
           </p>
           <p className="flex flex-row items-center gap-3">
-            <FontAwesomeIcon
-              icon={faStar}
-              className="text-yellow-500 animate-pulse"
-            />
-            <span className="font-semibold text-purple-800">Level:</span>
+            <i className="fas fa-cogs text-yellow-700"></i>
+            <span className="font-semibold text-yellow-700">Level:</span>
             <span className="text-gray-700">{participant.level}</span>
           </p>
         </div>
       </div>
 
-      {/* Container untuk Chart dengan Flex */}
-      <div className="flex flex-col items-center gap-6">
-        <div className="flex flex-col md:flex-row justify-center items-center gap-6 w-full">
-          {talent?.length > 0 ? (
-            <>
-              <div className="w-full md:w-1/2 bg-gradient-to-r from-blue-50 to-teal-200 p-4 rounded-lg shadow-md h-[400px] flex items-center justify-center">
-                <div className="w-full h-full">
-                  <Doughnut
-                    data={doughnutData}
-                    options={{ maintainAspectRatio: false }}
-                  />
-                </div>
-              </div>
-              <div className="w-full md:w-1/2 bg-gradient-to-l from-blue-50 to-emerald-200 p-4 rounded-lg shadow-md h-[400px] flex items-center justify-center">
-                <div className="w-full h-full">
-                  <Bar
-                    data={barData}
-                    options={{ ...options, maintainAspectRatio: false }}
-                  />
-                </div>
-              </div>
-            </>
-          ) : (
-            <p className="text-center text-gray-500">Loading data...</p>
-          )}
+      <div className="flex flex-col md:flex-row items-center gap-6 w-full">
+        <div className="w-full md:w-1/2 bg-gradient-to-r from-yellow-100 via-orange-100 to-yellow-200 p-4 rounded-lg shadow-md h-[400px] flex items-center justify-center">
+          <div className="w-full h-full">
+            <Bar
+              data={barData}
+              options={{ ...options, maintainAspectRatio: false }}
+            />
+          </div>
         </div>
 
-        {/* Progress Bar Section */}
-        <div className="w-full text-center mt-3">
-          {topThreeTalents.map((talent, index) => (
-            <div key={index} className="w-full mb-2">
-              <p className="text-gray-700 font-semibold mb-1">{talent.name}</p>
-              <div className="w-full bg-gray-200 rounded-full h-6 overflow-hidden">
-                <div
-                  className="h-full text-center text-white text-xs leading-6"
-                  style={{
-                    width: `${(talent.score / 10) * 100}%`,
-                    backgroundColor: ["#4bc0c0", "#f39c12", "#e74c3c"][index],
-                  }}
-                ></div>
-              </div>
+        {/* Progress chart akan tersembunyi di bawah layar md (tablet ke bawah) */}
+        <div className="w-full md:w-1/2 bg-gradient-to-l from-yellow-100 via-orange-200 to-red-100 p-4 rounded-lg shadow-md h-[400px] flex items-center justify-center hidden md:block">
+          <div className="w-full h-full">
+            <div className="space-y-4 w-full">
+              {topThreeTalents.map((talent, index) => (
+                <div key={index} className="w-full">
+                  <p className="text-gray-700 font-semibold mb-1">
+                    <i className="fas fa-star text-yellow-600"></i>{" "}
+                    {talent.name}
+                  </p>
+                  <div className="w-full bg-gray-200 rounded-full h-6 overflow-hidden">
+                    <div
+                      className="h-full text-center text-white text-xs leading-6"
+                      style={{
+                        width: `${(talent.score / 10) * 100}%`,
+                        backgroundColor: [
+                          "#ff9f40", // Orange
+                          "#f39c12", // Yellow
+                          "#f9b5b1", // Light Red
+                        ][index],
+                      }}
+                    ></div>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
       </div>
+
       <div className="mb-6">
-        <h3 className="font-semibold text-blue-600 my-5">Rekomendasi:</h3>
+        <h3 className="font-semibold text-orange-600 my-5">
+          <i className="fas fa-lightbulb text-xl text-yellow-500"></i>{" "}
+          Rekomendasi:
+        </h3>
         {topThreeTalents.map((talentItem, index) => (
           <div
             key={index}
-            className="p-4 mb-4 bg-blue-50 rounded-md hover:bg-blue-100 transition duration-300"
+            className="p-4 mb-4 bg-orange-50 rounded-md hover:bg-orange-100 transition duration-300"
           >
             <div className="flex justify-between items-center">
-              <h4 className="text-blue-600 text-xl font-semibold">
+              <h4 className="text-orange-600 text-xl font-semibold">
                 {talentItem.name}
               </h4>
               <button
                 onClick={() => toggleExpand(index)}
-                className="text-blue-600 font-semibold transition duration-200 flex items-center gap-1"
+                className="text-orange-600 font-semibold transition duration-200 flex items-center gap-1"
               >
                 {expanded === index ? (
                   <>
-                    <FontAwesomeIcon icon={faChevronUp} /> Sembunyikan
+                    <i className="fas fa-minus-circle"></i> Sembunyikan
                   </>
                 ) : (
                   <>
-                    <FontAwesomeIcon icon={faChevronDown} /> Lihat Detail
+                    <i className="fas fa-plus-circle"></i> Lihat Detail
                   </>
                 )}
               </button>
@@ -213,7 +199,7 @@ const ResultQuizCard = ({ quizResult }) => {
             <div
               className={`mt-4 overflow-hidden transition-all duration-500 ease-in-out ${
                 expanded === index
-                  ? "max-h-[400px] opacity-100"
+                  ? "max-h-[400px] opacity-100 overflow-y-auto"
                   : "max-h-0 opacity-0"
               }`}
             >
@@ -239,46 +225,42 @@ const ResultQuizCard = ({ quizResult }) => {
           </div>
         ))}
 
-        {/* Profesi */}
-        <h3 className="font-semibold text-blue-600 my-5">
-          Profesi yang Direkomendasikan:
+        <h3 className="font-semibold text-orange-600 my-5">
+          <i className="fas fa-briefcase text-xl text-yellow-500"></i> Profesi
+          yang Direkomendasikan:
         </h3>
         {profession.map((item, index) => (
           <div
             key={index}
-            className="p-4 mb-4 bg-green-50 rounded-md hover:bg-green-100 transition duration-300"
+            className="p-4 mb-4 bg-yellow-50 rounded-md hover:bg-yellow-100 transition duration-300"
           >
-            <h4 className="text-green-600 text-xl font-semibold">
-              {item.name}
+            <h4 className="text-yellow-600 text-xl font-semibold">
+              <i className="fas fa-briefcase"></i> {item.name}
             </h4>
           </div>
         ))}
 
-        {/* Jurusan */}
-        <h3 className="font-semibold text-blue-600 my-5">
-          Jurusan yang Direkomendasikan:
+        <h3 className="font-semibold text-orange-600 my-5">
+          <i className="fas fa-school text-xl text-yellow-500"></i> Jurusan yang
+          Direkomendasikan:
         </h3>
         {major.map((item, index) => (
           <div
             key={index}
-            className="p-4 mb-4 bg-yellow-50 rounded-md hover:bg-yellow-100 transition duration-300"
+            className="p-4 mb-4 bg-orange-50 rounded-md hover:bg-orange-100 transition duration-300"
           >
             <div className="flex justify-between items-center">
-              <h4 className="text-yellow-600 text-xl font-semibold">
-                {item.name}
+              <h4 className="text-orange-600 text-xl font-semibold">
+                <i className="fas fa-graduation-cap"></i> {item.name}
               </h4>
               <button
                 onClick={() => toggleExpand(`major-${index}`)}
-                className="text-yellow-600 font-semibold transition duration-200 flex items-center gap-1"
+                className="text-orange-600 font-semibold transition duration-200 flex items-center gap-1"
               >
                 {expanded === `major-${index}` ? (
-                  <>
-                    <FontAwesomeIcon icon={faChevronUp} /> Sembunyikan
-                  </>
+                  <i className="fas fa-minus-circle"></i>
                 ) : (
-                  <>
-                    <FontAwesomeIcon icon={faChevronDown} /> Lihat Detail
-                  </>
+                  <i className="fas fa-plus-circle"></i>
                 )}
               </button>
             </div>
