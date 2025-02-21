8ad22c5a799ca828c6b0b7e6a066c04640b3ab58
Ananta Pirdhaus <ananta.firdaus2@gmail.com>
2025-02-21 18:50:49 +0700

    updated: footer

--- src/common/footer.jsx
+++ src/common/footer.jsx
@@ -30,7 +30,7 @@ export default function Footer() {
   return (
     <footer className="bg-white border-t border-light-gray" id="contact">
       {/* Footer Top Section */}
-      <div className="py-10 container mx-auto grid gap-8 lg:grid-cols-3 text-center lg:text-left">
+      <div className="py-10 container mx-auto grid gap-8 lg:grid-cols-2 text-center lg:text-left">
         {/* Brand Info */}
         <div className="max-w-md mx-auto lg:mx-0 text-dark-gray">
           <a href="#" className="block mb-4">
@@ -82,30 +82,6 @@ export default function Footer() {
               </ul>
             )}
           </div>
-        </div>
-
-        {/* Newsletter */}
-        <div className="max-w-md mx-auto lg:mx-0 text-dark-gray">
-          <h5 className="mb-4 uppercase text-lg font-bold">Newsletter</h5>
-          <p className="text-gray-600">
-            Subscribe untuk mendapatkan update & berita terbaru.
-          </p>
-          <form className="mt-4 flex w-full max-w-sm">
-            <input
-              type="email"
-              className="flex-1 px-4 py-2 bg-light-gray text-dark-gray border border-gray-300 outline-none"
-              placeholder="Your email address"
-            />
-            <motion.button
-              type="submit"
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-              className="bg-orange-500 px-4 text-white flex items-center justify-center transition duration-300"
-              aria-label="Subscribe"
-            >
-              <i className="fa-solid fa-paper-plane"></i>
-            </motion.button>
-          </form>
         </div>
       </div>
     </footer>
