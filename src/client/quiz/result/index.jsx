bf94ced68df4f935bbd9d92b4c9531d07a6b29b9
Ananta Pirdhaus <ananta.firdaus2@gmail.com>
2025-02-22 17:18:03 +0700

    update: result

--- src/client/quiz/result/index.jsx
+++ src/client/quiz/result/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import ResultQuizCard from "./components/ResultQuzCard";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -22,8 +22,8 @@ const fetchResultQuizData = async (idSession) => {
 export default function ResultQuizApp() {
   const [quizResult, setQuizResult] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
   const { idSession } = useParams();
+  const navigate = useNavigate(); // Menambahkan useNavigate
 
   useEffect(() => {
     const initializeResultQuiz = async () => {
@@ -31,18 +31,24 @@ export default function ResultQuizApp() {
         const fetchedResult = await fetchResultQuizData(idSession);
         if (!fetchedResult) {
           toast.error("Tidak ada data hasil kuis yang tersedia.");
+          setTimeout(() => {
+            navigate(`/survey/${idSession}`); // Redirect ke halaman survey
+          }, 2000); // Delay 2 detik agar user melihat pesan error
         } else {
           setQuizResult(fetchedResult);
         }
       } catch (error) {
-        setError("Gagal memuat data hasil kuis.");
+        toast.error("Gagal memuat data hasil kuis.");
+        setTimeout(() => {
+          navigate(`/survei/${idSession}`);
+        }, 2000);
       } finally {
         setLoading(false);
       }
     };
 
     initializeResultQuiz();
-  }, [idSession]);
+  }, [idSession, navigate]);
 
   if (loading) {
     return (
@@ -53,16 +59,12 @@ export default function ResultQuizApp() {
     );
   }
 
-  if (error) {
-    return <div className="text-center text-red-600">{error}</div>;
-  }
-
   return (
     <div className="flex flex-col h-screen">
       <main className="p-3 mt-5 flex-grow items-center justify-center">
         <div className="p-3">
           <ResultQuizCard
-            quizResult={quizResult} // Mengirimkan data hasil kuis lengkap
+            quizResult={quizResult}
             idSession={idSession}
             baseUrl={BASE_URL}
           />
